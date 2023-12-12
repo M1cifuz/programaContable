99cdf3c43fdb8664e5f28c31f2f1ed79a250030d
Camilo Saenz <titanasura@gmail.com>
2023-12-12 18:45:31 -0500

    Agregando los botones

--- casaroma.py
+++ casaroma.py
@@ -1,27 +1,77 @@
-#programa contable estandar casaroma
+# Gestor base de datos productos
 from tkinter import *
 from tkinter import messagebox
 import sqlite3
 #----------inicio Modulo interfaz grafica----------
 root = Tk()
-root.title("Programa contable") # titulo de la ventana 
+root.title("Gestor Base de datos productos") # titulo de la ventana 
+root.resizable(False,False) # Controla el poder redimensionar la ventana
+root.geometry("420x200") # Establece el tamaño de la ventana principal
+root.config(bg="#E48F45")
 #----- inicio primer frame-----
 miFrame=Frame(root, width=350, height=350)
+miFrame.config(bg="#E48F45")
 miFrame.pack()
 #-----fin primer frame-----
 
 #----- inicio labels-----
-labelConsecutivo = Label(miFrame, text="Consecutivo")
-labelConsecutivo.grid(row=0, column=0, sticky="e")
+labelIDProducto = Label(miFrame, text="ID producto:", font=(12))
+labelIDProducto.grid(row=0, column=0, sticky="e", pady=5, padx=3)
+labelIDProducto.config(bg="#E48F45")
 
-textoConsecutivo = Entry(miFrame)
-textoConsecutivo.grid(row=0, column=1)
+textoIDProducto = Entry(miFrame)
+textoIDProducto.grid(row=0, column=1)
 
-labelFecha = Label(miFrame, text="Fecha")
-labelFecha.grid(row=1, column=0, sticky="e")
+labelProducto = Label(miFrame, text="Producto:", font=(12))
+labelProducto.grid(row=1, column=0, sticky="e", pady=5, padx=3)
+labelProducto.config(bg="#E48F45")
+
+textoProducto = Entry(miFrame)
+textoProducto.grid(row=1, column=1)
+
+labelMarca = Label(miFrame, text="Marca:", font=(12))
+labelMarca.grid(row=1, column=2, sticky="e", pady=5, padx=3)
+labelMarca.config(bg="#E48F45")
+
+textoMarca = Entry(miFrame)
+textoMarca.grid(row=1, column=3)
+
+labelPresentacion = Label(miFrame, text="Presentaciòn:", font=(12))
+labelPresentacion.grid(row=2, column=0, sticky="e", pady=5, padx=3)
+labelPresentacion.config(bg="#E48F45")
+
+textoPresentacion = Entry(miFrame)
+textoPresentacion.grid(row=2, column=1)
+
+labelPrecio = Label(miFrame, text="Precio:", font=(12))
+labelPrecio.grid(row=2, column=2, sticky="e", pady=5, padx=3)
+labelPrecio.config(bg="#E48F45")
 
 textoFecha = Entry(miFrame)
-textoFecha.grid(row=1, column=1)
+textoFecha.grid(row=2, column=3)
+
+#-----Botones-----
+#----------Crear tabla----------
+def crearTabla ():
+    miConexion = sqlite3.connect("productos")   # crear una conexiòn
+    miCursor = miConexion.cursor()    # crear puntero
+    miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_PRODUCTO VARCHAR(40), MARCA_PRODUCTO VARCHAR(40), PRESENTACION INTEGER, PRECIO INTEGER)")
+
+botonCrear = Button(miFrame, text="Crear", font=10, command=crearTabla)
+botonCrear.grid(row=3, column=0, sticky="w")
+botonCrear.config(bg="#994D1C")
+
+botonLeer = Button(miFrame, text="Leer", font=10)
+botonLeer.grid(row=4, column=0, sticky="w")
+botonLeer.config(bg="#994D1C")
+
+botonActualizar = Button(miFrame, text="Actualizar", font=10)
+botonActualizar.grid(row=3, column=1, sticky="w")
+botonActualizar.config(bg="#994D1C")
+
+botonBorrar = Button(miFrame, text="Borrar", font=10)
+botonBorrar.grid(row=4, column=1, sticky="w")
+botonBorrar.config(bg="#994D1C")
 #----- fin labels-----
 
 root.mainloop()
@@ -31,11 +81,7 @@ root.mainloop()
 
 #----------INICIO FUNCIONES----------
 #----------Crear bases de datos----------
-#----------Crear tabla----------
-def crearTabla ():
-    miConexion = sqlite3.connect("productos")   # crear una conexiòn
-    miCursor = miConexion.cursor()    # crear puntero
-    miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_PRODUCTO VARCHAR(50), PRECIO INTEGER, PROVEEDOR VARCHAR(20))")
+
 #----------Funciones CRUD para listas----------
 
 def crearProducto():
